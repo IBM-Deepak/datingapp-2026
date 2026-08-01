89bc42b99b1404a2b4d04c5e16c8581882e77326
Deepak.Palsamkar@brother.co.uk <Deepak.Palsamkar@brother.co.uk>
2026-08-02 03:39:18 +0530

    Section 4

---
+++ API/Controllers/BaseApiController.cs
@@ -0,0 +1,12 @@
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")] //localhost:5001/api/BaseApi
+    [ApiController]
+    public class BaseApiController : ControllerBase
+    {
+        
+    }
+}
